74913632c05f8b2e1d5f7cf1fea7681dad68482d
Vincenzo Ingrao Jr. <vincenzoingraojr@gmail.com>
2025-06-10 20:04:48 +0200

    fix: dot in a username

--- src/regexp/validMentionOrList.js
+++ src/regexp/validMentionOrList.js
@@ -8,7 +8,7 @@ import validMentionPrecedingChars from './validMentionPrecedingChars';
 const validMentionOrList = regexSupplant(
   '(#{validMentionPrecedingChars})' + // $1: Preceding character
   '(#{atSigns})' + // $2: At mark
-  '([a-zA-Z0-9_]{1,20})' + // $3: Screen name
+  '([a-zA-Z0-9_](?:[a-zA-Z0-9_.]{0,18}[a-zA-Z0-9_])?)' + // $3: Screen name
     '(/[a-zA-Z][a-zA-Z0-9_-]{0,24})?', // $4: List (optional)
   { validMentionPrecedingChars, atSigns },
   'g'
